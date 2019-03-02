2d62288df6fac2e1da4f8f11d43360d26e029a83
HPLegion <32222819+HPLegion@users.noreply.github.com>
2019-03-02 17:04:19 +0100

    commi

--- scrape_game_info.py
+++ scrape_game_info.py
@@ -1,8 +1,14 @@
 import pandas as pd
 
-df = pd.read_csv("games_input.csv")
+df = pd.read_csv("games_input.csv", dtype=str, na_values=str)
 
 def get_id(url):
-    
+    try:
+        return url.split("/")[4]
+    except:
+        return ""
 
-df["BGG_ID"] = 
+df["BGG_ID"] = df["BGG_URL"].apply(get_id)
+
+for i, row in df.iterrows():
+    print(row.BGG_ID)
